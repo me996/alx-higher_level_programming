3a0dc88ef8aebc335ba9834feaa2566093691490
me996 <me996@gmail.com>
2023-10-11 14:36:36 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,16 @@
 if __name__ == "__main__":
     import sys
 
+    # Get the number of command-line arguments (excluding the script name itself)
     count = len(sys.argv) - 1
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
+
+    # Loop through the command-line arguments and print them
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
