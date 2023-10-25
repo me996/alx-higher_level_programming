f7b83be281e523d858ebf86050072ade20d3ded2
me996 <me996@gmail.com>
2023-10-25 07:22:26 -0700

    OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""square module"""
+
+
 class Square:
+    """This is the Square class."""
+
     def __init__(self, size):
+        """Initialize a new Square instance with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            None
+        """
         self.__size = size
