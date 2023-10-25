f969922854d8be358e7575797be40ed8f5c7ba48
me996 <me996@gmail.com>
2023-10-25 07:37:56 -0700

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""square module."""
+
+
 class Square:
+    """define a square."""
+
     def __init__(self, size=0):
+        """constructor.
+
+        Args:
+             size: lenght of a side of the square.
+
+        Raises:
+             TypeError: If size is not an integer
+             ValueError: If size is less than 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
