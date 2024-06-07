9745771bbba2390e525c88f618b766dc2a116fdb
me996 <me996@gmail.com>
2024-06-07 23:03:15 +0100

    multi_languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,7 @@
 const firstline = 'c is fun';
 const secondline = 'Python is cool';
 const thirdline = 'JavaScript is amazing';
+
 console.log(firstline);
 console.log(secondline);
 console.log(thirdline);
