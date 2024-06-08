6a69c484b9fd9e98f828aa008f9ca525106cedb8
me996 <me996@gmail.com>
2024-06-08 23:02:31 +0100

    2-argV

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 /* script that prints a message depending of the number of arguments passed */
-
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
@@ -9,4 +8,3 @@ if (process.argv.length === 2) {
 } else {
   console.log('Argument found');
 }
-
