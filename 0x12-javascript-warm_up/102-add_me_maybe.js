6099f060f4960a3abe04b0066be69ba42801524c
me996 <me996@gmail.com>
2024-06-09 19:24:51 +0100

    102_add_me

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 /* function that increments and calls a function. */
-exports.addMeMaybe = function(number, theFunction) {
-    theFunction(++number);
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
 };
