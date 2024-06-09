a85918ea7eeae0f1e31aca483cceb9c92817ef98
me996 <me996@gmail.com>
2024-06-09 17:04:34 +0100

    10_factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 /* script that computes and prints a factorial */
-function factorial(n) {
-        if (isNaN(n)){
-        return 1;
-}
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-        return n * factorial(n - 1);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
