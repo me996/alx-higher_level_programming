211aefb3f0ffc51004fe79faec0e5419f0215f7e
me996 <me996@gmail.com>
2024-06-09 01:52:47 +0100

    6_multi_languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/*  script that prints 3 lines by using an array of string and a loop */
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
