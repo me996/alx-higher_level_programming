1efa11e748cbe9cde1d0f0a9377b025ecad36fc3
me996 <me996@gmail.com>
2024-06-09 03:19:17 +0100

    8_square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
 /* script that prints a square */
-if ((process.argv[2]) == undefined || isNaN (process.argv[2])) {
+if ((process.argv[2]) === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const x = Number (process.argv[2]);
+  const x = Number(process.argv[2]);
   let i = 0;
   while (i < x) {
-  console.log('x' .repeat(x));
-  i++;
+    console.log('x'.repeat(x));
+    i++;
+  }
 }
-}
-
